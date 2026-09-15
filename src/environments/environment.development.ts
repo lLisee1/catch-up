320b322200faf1873451503a98ddeb38bb2198b1
alisee <u202624323@upc.edu.pe>
2026-09-15 11:34:53 -0500

    feat(environment):  add environment files for development and production configurations

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,14 @@
+/**
+ * Development environment configuration for the application.
+ * This file contains settings and variables specific to the development environment.
+ * It is used to configure the application during development and testing.
+ */
+export const environment = {
+  production: false,
+  newsProviderApiBaseUrl: 'https://newsapi.org/v2',
+  newsProviderNewsEndpointPath: '/top-headlines',
+  newsProviderSourcesEndpointPath: '/top-headlines/sources',
+  newsProviderApiKey: '60d828f15966496bb41f77fe7fa6df99',
+  logoProviderApiBaseUrl: 'https://img.logo.dev',
+  logoProviderPublishableKey: 'pk_XN7hZs2CSkivbW2T3of9hA',
+};
